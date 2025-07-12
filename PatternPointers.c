79540bca46fd8eaefa1d0f20453d979dcf8eedf6
sai kiran pathivada <saip73191@gmail.com>
2025-07-12 06:09:59 +0530

    Update PatternPointers.c
    comments are added

--- PatternPointers.c
+++ PatternPointers.c
@@ -2,8 +2,10 @@
 #include <string.h>
 int main() {
     char *s="Github";
+    // s holds the address of the first character
     for (int i = 0;i < strlen(s); i++)
     {
+        // in this we are working on the pointer arithmetics by changing the starting position to next
         printf("%s\n",s+i);   
     }
     return 0;
